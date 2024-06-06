2a23cc5c9a735d9921ac4ccfc698037d344d4b19
Suyog-Kulkarni <kulkarnisuyog192@gmail.com>
2024-06-06 19:23:44 +0530

    Called all function in one

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -1,53 +1,56 @@
 package ui;
+
+import org.testng.annotations.Test;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
-
-import static org.openqa.selenium.support.locators.RelativeLocator.with;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
-	
-	@Test
-	public static void main(String []args) {
-		
-		
-		
-		Automation a = new Automation();
-		try {
-			a.LoginInApp();
-			a.AddtoCart();
-			//a.BackHome();
-			a.CheckCartItems();
-			a.Checkout();
-			//a.HambergerOperations();
-			a.TitleCheck();
-			a.Finish();
-			//a.RemoveFromCart();
-			a.Sorting();
-			a.ProductDeatil();
-			DragAndDrop.DandDfun();
-			MouseHover.MouseH();
-			SelectDropDown.SelectDD();
-			WebDriverMethods.WebDM();
-			
-			WebDriverManager.chromedriver().setup();
-			ChromeDriver driver = new ChromeDriver();
-			driver.get("https://demoqa.com/login");
-			WebElement password  = driver.findElement(By.id("password"));
-			password.sendKeys("Suyog@123");
-			driver.findElement(with(By.tagName("input")).above(password)).sendKeys("Suyog");
-			//driver.findElement(with(By.tagName("button")).below(password)).click();
-			driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}catch(ElementClickInterceptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
 
+    @Test
+    public void fullTestFlow() {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/login");
+
+        // Assuming Automation class has a method to handle login
+        Automation a = new Automation();
+        try {
+            a.LoginInApp();
+            a.AddtoCart();
+            //a.BackHome(); // Uncomment if needed
+            a.CheckCartItems();
+            a.Checkout();
+            //a.HambergerOperations(); // Uncomment if needed
+            a.TitleCheck();
+            a.Finish();
+            //a.RemoveFromCart(); // Uncomment if needed
+            a.Sorting();
+            a.ProductDeatil();
+            DragAndDrop.DandDfun();
+            MouseHover.MouseH();
+            SelectDropDown.SelectDD();
+            WebDriverMethods.WebDM();
+
+            // Example of finding and interacting with elements
+            WebElement password  = driver.findElement(By.id("password"));
+            password.sendKeys("Suyog@123");
+            driver.findElement(with(By.tagName("input")).above(password)).sendKeys("Suyog");
+            driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+
+            // Add assertions or further actions as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ElementClickInterceptedException e) {
+            e.printStackTrace();
+        } finally {
+            // Ensure the driver is closed properly
+            driver.quit();
+        }
+    }
+
+    // Other test methods...
 }
