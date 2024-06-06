2489cc4f22f160519dafda0a718fc1a9c9677ef7
Suyog-Kulkarni <kulkarnisuyog192@gmail.com>
2024-06-06 17:56:28 +0530

    Called all function in one line

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -10,7 +10,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class LoginTest {
 	
 	@Test
-	public void fun(String[] args) {
+	public static void main(String[] args) {
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
